11f8ea8927f5d2082f18a6b9ac67cdf3a952bdeb
IsmVirusXD <igordesouzamendes@outlook.com>
2026-05-02 11:46:46 -0300

    refactor(trackerBuilder): change `useState` to `useMemo` in `textList\index.tsx`

--- app/features/trackerBuilder/components/textList/index.tsx
+++ app/features/trackerBuilder/components/textList/index.tsx
@@ -1,29 +1,27 @@
 "use client";
 
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import TextListPresenter from "./presenter";
 
 interface TextListProps {
   title: string;
-  info?: string ;
+  info?: string;
   items: string[];
 }
 
 export default function TextList({ title, info, items }: TextListProps) {
-  
   const idBase = title.toLocaleLowerCase().replace(/\s+/g, "");
 
   const [itemDone, setItemDone] = useState<boolean[]>(
     new Array(items.length).fill(false),
   );
-  const [isListComplete, setIsListComplete] = useState<boolean>(false);
+  const isListComplete = useMemo(() => itemDone.every(Boolean), [itemDone]);
   const [showInfo, setShowInfo] = useState<boolean>(true);
 
   const toggleItemDoneIndex = (index: number) => {
     const newState = [...itemDone];
     newState[index] = !newState[index];
     setItemDone(newState);
-    setIsListComplete(newState.every(Boolean));
   };
 
   const checkIsItemDoneIndex = (index: number): boolean => {
